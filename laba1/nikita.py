c1bbb1f27c2ff389bd61a52a4511a35d341dfccd
n0w3e <5ejyjvdldwfk@mail.ru>
2024-03-15 22:08:05 +0300

    Revert "delete 1 string"
    This reverts commit 3df3fc4ff4f92e501ee43f5992821b4d6d3fdbf0.

--- laba1/nikita.py
+++ laba1/nikita.py
@@ -1,8 +1,12 @@
 print("Hello, IT")
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 print("123")
 >>>>>>> parent of 8e95cb2 (delete 1 string)
 =======
 >>>>>>> parent of 3df3fc4 (add 1 string)
+=======
+print("123")
+>>>>>>> parent of 8e95cb2 (delete 1 string)

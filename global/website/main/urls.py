aa3bafb912158f7eeaa3a31a2e7e2f24dd1e6b23
rehcrr <ornstein0882@gmail.com>
2024-05-10 01:24:28 +0300

    commit_Alex_10_05_2024

--- global/website/main/urls.py
+++ global/website/main/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 from . import views
 from .views import register
+from .views import reset_password
+from .views import password_change
 from .views import UserProfileView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index),
     path('register/', register, name='register'),
+    path('reset_password/', reset_password, name='reset_password'),
     path('login/', views.login_view, name='login'),
     path('profile/', UserProfileView.as_view(), name='user_profile'),
     path('logout/', views.logout_view, name='logout'),
@@ -18,7 +22,9 @@ urlpatterns = [
     path('remove_from_cart/<int:pizza_id>/', views.remove_from_cart, name='remove_from_cart'),
     path('cart/', views.cart, name='cart'),
     path('go_to_cart/', views.go_to_cart, name='go_to_cart'),
+    path('password_change/<uidb64>/<token>/', views.password_change, name='password_change')
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
